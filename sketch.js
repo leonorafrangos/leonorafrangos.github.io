a9a45779659052db0888655e6813912f8d3dbe1a
leonorafrangos <104769383+leonorafrangos@users.noreply.github.com>
2022-05-11 09:53:25 -0400

    adding ellipses project
    added all the files

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(500, 500);
+  background(0);
+}
+
+function draw() {
+  fill('white');
+  ellipse(30, 100, 100);
+  fill('pink');
+  ellipse(30, 200, 200);
+  fill('green');
+  ellipse(30, 300, 300);
+  fill('blue');
+  ellipse(30, 400, 400);
+}
