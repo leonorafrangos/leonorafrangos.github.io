700ce4fb81314ea3b6c7f403209a2d30552e2206
leonorafrangos <104769383+leonorafrangos@users.noreply.github.com>
2022-05-26 08:32:27 -0400

    updatte

--- sketch.js
+++ sketch.js
@@ -1,15 +1,4 @@
 function setup() {
-  createCanvas(500, 500);
-  background(0);
-}
-
-function draw() {
-  fill('white');
-  ellipse(30, 100, 100);
-  fill('pink');
-  ellipse(30, 200, 200);
-  fill('green');
-  ellipse(30, 300, 300);
-  fill('blue');
-  ellipse(30, 400, 400);
+  background(238, 157, 250);
+  createCanvas(windowWidth, windowHeight);
 }
